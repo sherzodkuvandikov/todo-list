1bb9baddbb7e9c34b608524de87b786adc2ab0f2
MuhammadSiddiq <707myworld@gmail.com>
2020-11-03 16:43:09 +0500

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var mainForm = document.querySelector('.main-form');
+var mainInput = mainForm.querySelector('.main-input').value;
+var mainCheckbox = mainForm.querySelector('.main-checkbox').value;
+var mainList = document.querySelector('.main-list');
+
+mainForm.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+
+
+
+  console.log();
+})
